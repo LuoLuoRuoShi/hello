03a306be07ebb55b7f041273273a0a75423f7544
luoluoruoshi <1047433899@qq.com>
2017-08-09 16:30:08 +0800

    First commit

---
+++ src/sayHello.php
@@ -0,0 +1,10 @@
+<?php
+namespace Hello;
+
+class sayHello
+{
+	public static function hello()
+	{
+		return 'hello,world';
+	}
+}
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+require "./vendor/autoload.php";
+use Hello\sayHello;
+echo sayHello::hello();
